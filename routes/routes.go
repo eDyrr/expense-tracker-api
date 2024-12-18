3be01feb3b4614e88c5f368ce32b30acaf261133
eDyrr <e6yr@proton.me>
2024-12-18 15:22:25 +0100

    improved the routing

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/eDyrr/expense-tracker-api/controllers"
 	"github.com/eDyrr/expense-tracker-api/middleware"
+	"github.com/eDyrr/expense-tracker-api/templates"
 	"github.com/gorilla/mux"
 )
 
@@ -12,15 +13,20 @@ func SetUpRoutes(router *mux.Router, tmpl *template.Template) {
 
 	// API routes
 	// apiRouter := router.PathPrefix("/api").Subrouter()
-	router.HandleFunc("/", controllers.WithTemplate(tmpl, controllers.Index))
-	router.HandleFunc("/signup", controllers.WithTemplate(tmpl, controllers.SignUp))
+	router.HandleFunc("/", templates.WithTemplate(tmpl, templates.Index))
+	router.HandleFunc("/signup", templates.WithTemplate(tmpl, templates.SignUp))
+	router.HandleFunc("/login", templates.WithTemplate(tmpl, templates.Login))
 	// apiRouter.HandleFunc("/login", controllers.Login)
 	// apiRouter.HandleFunc("/auth", controllers.WithTemplate(tmpl, controllers.SignUp))
+
+	api := router.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/signup", controllers.SignUp)
+
 	homeMiddleware := router.PathPrefix("/site").Subrouter()
 	homeMiddleware.Use(middleware.Auth)
 
 	homeMiddleware.HandleFunc("/logout", controllers.Logout)
-	homeMiddleware.HandleFunc("/home", controllers.WithTemplate(tmpl, controllers.Home))
+	homeMiddleware.HandleFunc("/home", templates.WithTemplate(tmpl, templates.Home))
 	homeMiddleware.HandleFunc("/list", controllers.Listall)
 	homeMiddleware.HandleFunc("/purchase", controllers.AddPurchase)
 }
